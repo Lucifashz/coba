740c9d0a71394ef4dced22f6efae307a1c3440f9
Achmad Rangga <achrangga44@gmail.com>
2024-08-30 19:06:37 +0700

    second commit

--- api/routes/Paypal.js
+++ api/routes/Paypal.js
@@ -1,6 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const router = express.Router();
+
+dotenv.config();
 
 router.get('/api/config/paypal', (req, res) => {
    res.send(process.env.PAYPAL_CLIENT_ID);
