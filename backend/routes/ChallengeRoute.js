addd747b4e9082c33cbe504a7bde5677c72279af
Achmad Rangga <achrangga44@gmail.com>
2024-09-03 12:27:24 +0700

    s commit

---
+++ backend/routes/ChallengeRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { 
+   createChallenge, getChallanges, 
+   getChallangesByChallengedId, getChallangesByChallengerId,
+   updateChallengeByChallanged, updateChallengeByChallanger,
+   updateChallengeMatchStatusByChallanged
+} from "../controllers/ChallengeController.js";
+
+
+const router = express.Router();
+
+router.post('/challenge', createChallenge);
+router.get('/challenges', getChallanges);
+router.get('/challenges-by-challenged-id/:id', getChallangesByChallengedId);
+router.get('/challenges-by-challenger-id/:id', getChallangesByChallengerId);
+router.patch('/challenges-by-challenged-id/:id', updateChallengeByChallanged);
+router.patch('/challenges-by-challenger-id/:id', updateChallengeByChallanger);
+router.patch('/challenges-match-status/:id', updateChallengeMatchStatusByChallanged);
+
+export default router;
